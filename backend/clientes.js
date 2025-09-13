97219bc2a68dff63093c21b78c53a7b68558a0d1
daniela <daniela265@gmail.com>
2025-09-13 20:24:56 -0300

    Inicio

---
+++ backend/clientes.js
@@ -0,0 +1,23 @@
+[
+  {
+    "id": 1,
+    "nombre": "Cerrajería El Candado",
+    "contacto": "Juan Pérez",
+    "telefono": "221-456789",
+    "email": "candado@correo.com"
+  },
+  {
+    "id": 2,
+    "nombre": "Llaves Rápidas",
+    "contacto": "María Gómez",
+    "telefono": "221-123456",
+    "email": "llaves@correo.com"
+  },
+  {
+    "id": 3,
+    "nombre": "PampaNova Distribuciones",
+    "contacto": "Pedro Rodríguez",
+    "telefono": "221-987654",
+    "email": "pampanova@correo.com"
+  }
+]
